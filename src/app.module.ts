34df0dc7d503080c0fa98de3b32a30a9931d52c0
vlbras <vlad.bras02@gmail.com>
2023-11-10 20:51:27 +0100

    feat: integrate Prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 @Module({
   imports: [],
-  providers: [],
+  providers: [PrismaService],
 })
 export class AppModule {}
