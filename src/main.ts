f563fbf26da34ff9420baa6fc0cd7df0f4a40a52
vlbras <vlad.bras02@gmail.com>
2023-11-10 21:30:50 +0100

    chore: use APP_PORT from .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  await app.listen(process.env.APP_PORT);
+  
+  Logger.log(
+    `Running on http://localhost:${process.env.APP_PORT}`,
+    'Bootstrap',
+  );
 }
 bootstrap();
