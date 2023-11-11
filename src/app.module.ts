89f55212a6451253b7d3d2656bd54f424137d0a9
vlbras <vlad.bras02@gmail.com>
2023-11-11 13:40:11 +0100

    feat: integrate GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      sortSchema: true,
+      playground: process.env.NODE_ENV !== 'prod',
+      introspection: process.env.NODE_ENV !== 'prod',
+    }),
+  ],
   providers: [PrismaService],
 })
 export class AppModule {}
