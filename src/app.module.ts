a55c3fe6afe0c4a3df3ca3b1f8307c37f459214a
vlbras <vlad.bras02@gmail.com>
2023-11-16 20:19:51 +0100

    feat: integrate Redis

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,8 @@ import { join } from 'path';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
 import { AccessTokenGuard } from './lib/guards';
+import { CacheModule } from '@nestjs/cache-manager';
+import { redisStore } from 'cache-manager-redis-yet';
 
 @Module({
   imports: [
@@ -15,6 +17,13 @@ import { AccessTokenGuard } from './lib/guards';
       sortSchema: true,
       playground: process.env.NODE_ENV !== 'prod',
       introspection: process.env.NODE_ENV !== 'prod',
+    }),
+    CacheModule.register({
+      isGlobal: true,
+      store: redisStore,
+      host: process.env.REDIS_HOST,
+      port: process.env.REDIS_PORT,
+      ttl: eval(process.env.REDIS_TTL),
     }),
     UserModule,
     AuthModule,
